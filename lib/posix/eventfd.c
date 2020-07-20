f964b963b4207f5084b3ca9744ef4531c1142e36
Rafał Kuźnia <rafal.kuznia@nordicsemi.no>
2020-07-20 23:20:26 +0200

    work in progress commit
    Signed-off-by: Rafał Kuźnia <rafal.kuznia@nordicsemi.no>

--- lib/posix/eventfd.c
+++ lib/posix/eventfd.c
@@ -5,14 +5,13 @@
  */
 
 #include <zephyr.h>
-
 #include <posix/sys/eventfd.h>
-
 #include <net/socket.h>
 
 struct eventfd {
 	struct k_sem read_sem;
 	struct k_sem write_sem;
+	struct k_mutex cnt_mtx;
 	eventfd_t cnt;
 	int flags;
 };
@@ -66,41 +65,84 @@ static int eventfd_poll_update(struct eventfd *efd,
 static ssize_t eventfd_read_op(void *obj, void *buf, size_t sz)
 {
 	struct eventfd *efd = obj;
+	bool nonblock = efd->flags & EFD_NONBLOCK;
+	int result = 0;
+	bool givesem = false;
 	eventfd_t count;
-	int ret = 0;
+	bool repeat;
 
 	if (sz < sizeof(eventfd_t)) {
 		errno = EINVAL;
 		return -1;
 	}
 
-	if (efd->cnt == 0) {
-		if (efd->flags & EFD_NONBLOCK) {
-			ret = -EAGAIN;
-		} else {
-			ret = k_sem_take(&efd->read_sem, K_FOREVER);
+	/* Reset the read_sem unconditionally. */
+	k_sem_take(&efd->read_sem, K_NO_WAIT);
+
+	do {
+		repeat = false;
+		k_mutex_lock(&efd->cnt_mtx, K_FOREVER);
+		do {
+			if (nonblock && efd->cnt == 0) {
+				result = EAGAIN;
+				break;
+			}
+			else if (efd->cnt == 0) {
+				repeat = true;
+				break;
+			}
+			count = (efd->flags & EFD_SEMAPHORE) ? 1 : efd->cnt;
+			efd->cnt -= count;
+			if (efd->cnt != 0) {
+				k_sem_give(&efd->read_sem);
+			}
+		} while (0);
+		k_mutex_unlock(&efd->cnt_mtx);
+		if (repeat) {
+			k_sem_take(&efd->read_sem, K_FOREVER);
+			continue;
 		}
+	} while (repeat);
+
+	/* Give the semaphores outside of the mutex so we don't reschedule more than we have to. */
+	if (result == 0) {
+		k_sem_give(&efd->write_sem);
 	}
 
-	if (ret < 0) {
-		errno = -ret;
+
+	if (result != 0) {
+		errno = result;
 		return -1;
 	}
 
-	count = (efd->flags & EFD_SEMAPHORE) ? 1 : efd->cnt;
-	efd->cnt -= count;
+#if defined(__GNUC__)
+/*
+ * GCC cannot determine, if count variable is initialized or not during
+ * assignment with buf. However since we know it is in fact initialized,
+ * we can ignore this warning.
+ */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 	*(eventfd_t *)buf = count;
-	k_sem_give(&efd->write_sem);
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 	return sizeof(eventfd_t);
 }
 
+
 static ssize_t eventfd_write_op(void *obj, const void *buf, size_t sz)
 {
-	eventfd_t count;
-	int ret = 0;
-
 	struct eventfd *efd = obj;
+	bool nonblock = efd->flags & EFD_NONBLOCK;
+	int result = 0;
+	eventfd_t count;
+	bool overflow;
+	bool repeat;
 
 	if (sz < sizeof(eventfd_t)) {
 		errno = EINVAL;
@@ -114,21 +156,37 @@ static ssize_t eventfd_write_op(void *obj, const void *buf, size_t sz)
 		return -1;
 	}
 
-	if (UINT64_MAX - count <= efd->cnt) {
-		if (efd->flags & EFD_NONBLOCK) {
-			ret = -EAGAIN;
-		} else {
-			ret = k_sem_take(&efd->write_sem, K_FOREVER);
-		}
+	if (count == 0) {
+		return sizeof(eventfd_t);
 	}
 
-	if (ret < 0) {
-		errno = -ret;
+	do {
+		repeat = false;
+		k_mutex_lock(&efd->cnt_mtx, K_FOREVER);
+		do {
+			overflow = UINT64_MAX - count <= efd->cnt;
+			if (nonblock && overflow) {
+				result = EAGAIN;
+				break;
+			}
+			else if (overflow) {
+				repeat = true;
+				break;
+			}
+			efd->cnt += count;
+		} while (0);
+		k_mutex_unlock(&efd->cnt_mtx);
+		if (repeat) {
+			k_sem_take(&efd->write_sem, K_FOREVER);
+			continue;
+		}
+		k_sem_give(&efd->read_sem);
+	} while (repeat);
+
+	if (result != 0) {
+		errno = result;
 		return -1;
 	}
-
-	efd->cnt += count;
-	k_sem_give(&efd->read_sem);
 
 	return sizeof(eventfd_t);
 }
@@ -197,7 +255,9 @@ static const struct fd_op_vtable eventfd_fd_vtable = {
 int eventfd(unsigned int initval, int flags)
 {
 	struct eventfd *efd = NULL;
-	int i, fd;
+	int fd = -1;
+	int seminit;
+	int i;
 
 	if (flags & ~EFD_FLAGS_SET) {
 		errno = EINVAL;
@@ -205,12 +265,6 @@ int eventfd(unsigned int initval, int flags)
 	}
 
 	k_mutex_lock(&eventfd_mtx, K_FOREVER);
-
-	fd = z_reserve_fd();
-	if (fd < 0) {
-		k_mutex_unlock(&eventfd_mtx);
-		return -1;
-	}
 
 	for (i = 0; i < ARRAY_SIZE(efds); ++i) {
 		if (!(efds[i].flags & EFD_IN_USE)) {
@@ -220,20 +274,26 @@ int eventfd(unsigned int initval, int flags)
 	}
 
 	if (efd == NULL) {
-		z_free_fd(fd);
 		errno = ENOMEM;
-		k_mutex_unlock(&eventfd_mtx);
-		return -1;
+		goto exit_mtx;
+	}
+
+	fd = z_reserve_fd();
+	if (fd < 0) {
+		goto exit_mtx;
 	}
 
 	efd->flags = EFD_IN_USE | flags;
-	efd->cnt = 0;
-	k_sem_init(&efd->read_sem, 0, UINT32_MAX);
-	k_sem_init(&efd->write_sem, 0, UINT32_MAX);
+	efd->cnt = initval;
+	seminit = efd->cnt != 0;
+
+	k_sem_init(&efd->read_sem, seminit, 1);
+	k_sem_init(&efd->write_sem, 0, 1);
+	k_mutex_init(&efd->cnt_mtx);
 
 	z_finalize_fd(fd, efd, &eventfd_fd_vtable);
 
+exit_mtx:
 	k_mutex_unlock(&eventfd_mtx);
-
 	return fd;
 }
